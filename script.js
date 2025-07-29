3b9018977e60aec0f666265b544b9b813ac395dd
Ryan Kosko <ryankosko31@gmail.com>
2025-07-29 09:34:03 -0700

    Update Rio de Janeiro trip presentation with new images, improved styles, and refactor JavaScript for better functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-Reveal.initialize({
-    hash: true,
-});
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+import Notes from 'reveal.js/plugin/notes/notes.esm.js';
+
+let deck = new Reveal({
+   plugins: [ Markdown, Highlight, Notes ]
+})
+deck.initialize();
